cb280d9fdf2bc3715b1484c7d041bb0d55988b20
Yan-Fa li <yanfali@gmail.com>
2013-04-05 21:04:39 -0700

    Add a markdown Run fmt

--- src/handbrake/structs.go
+++ src/handbrake/structs.go
@@ -1,29 +1,29 @@
 package handbrake
-import (
-)
+
+import ()
 
 type AudioMeta struct {
-	Language string
-	Codec string
-	Channels float32
+	Language  string
+	Codec     string
+	Channels  float32
 	Frequency int
-	Bps int
+	Bps       int
 }
 
 type SubtitleMeta struct {
 	Language string
-	Type string
+	Type     string
 }
 
 type HandBrakeMeta struct {
-	Title string
-	Duration float64
-	Height int
-	Width int
+	Title       string
+	Duration    float64
+	Height      int
+	Width       int
 	Pixelaspect string
-	Aspect float32
-	Fps float32
-	Autocrop string
-	Audio []AudioMeta
-	Subtitle []SubtitleMeta
+	Aspect      float32
+	Fps         float32
+	Autocrop    string
+	Audio       []AudioMeta
+	Subtitle    []SubtitleMeta
 }
--- src/main.go
+++ src/main.go
@@ -1,6 +1,6 @@
 package main
-import (
-)
+
+import ()
 
 func init() {
 }
