4521c93cfb5b11e1691d357222e768e796b9a0a7
Yan-Fa li <yanfali@gmail.com>
2013-04-09 23:28:55 -0700

    Flesh out gomkv cli tool
    - Add basic integration
    - invoke HandBrakeCLI
    - pass output to parser
    - dump parsed output to console

--- src/gomkv/main.go
+++ src/gomkv/main.go
@@ -4,9 +4,12 @@ import (
 	"path/filepath"
 	"fmt"
 	"os"
+	"exec"
+	"handbrake"
 )
 
 var workingDir string
+var files []string
 
 func init() {
 	var err error
@@ -14,16 +17,25 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func main() {
 	workingDir = filepath.Clean(workingDir)
 	fmt.Println(workingDir)
 	mkv, err := filepath.Glob(workingDir + "/*.mkv")
+	if err != nil {
+		panic(err)
+	}
 	m4v, err := filepath.Glob(workingDir + "/*.m4v")
-	files := append(mkv, m4v...)
+	if err != nil {
+		panic(err)
+	}
+	files = append(mkv, m4v...)
 	if len(files) == 0 {
 		fmt.Fprintf(os.Stderr, "No mkv/m4v files found in path. Exiting.\n")
 		os.Exit(1)
 	}
-}
-
-func main() {
+	std, err := exec.Command("HandBrakeCLI", "-t0", "-i", files[0])
+	meta := handbrake.ParseOutput(std.Err)
+	fmt.Print(meta)
 }
